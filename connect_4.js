73662101a2273d09e381f3b02df0d99fff95f174
Peter Casey <casey.peter10@gmail.com>
2019-11-15 13:33:39 -0600

    modified:   connect_4.js

--- connect_4.js
+++ connect_4.js
@@ -48,6 +48,97 @@ evalColumnHeight(array, columnChosen){
     return columnTop;
 }
 
+/* 
+The check functions below are called as the gameboard array is looped through. If
+a 1(black piece) or a 2(red piece)  is found during looping. The check win functions
+will be called. If it is impossible for a win in a particular direction to occur, the 
+function will terminate early.
 
+For example, if a piece in the top left of the board is scanned any win condition
+involving up or left will be impossible, and will not be executed
+*/
 
+//all rows and columns are zero indexed
+checkUpWin(board, row, column, color){
+    //exit condition
+    if(row < 3){
+        return;
+    } else {
+        //check if array contents upward from (row, column) match the color of (row, column)
+        if(board[row - 1][column] == color && board[row - 2][column] == color &&  board[row - 3][column] == color){
+            //color wins
+        } else {
+            return;
+        }
+    }
+}
 
+checkDownWin(board, row, column, color){
+    //exit condition
+    if(row > 2){
+        return;
+    } else {
+        //check if array contents downwar from (row, column) match the color of (row, column)
+        if(board[row + 1][column] == color && board[row + 2][column] == color &&  board[row + 3][column] == color){
+            //color wins
+        } else {
+            return;
+        }
+    }
+}
+
+checkUpRightDiag(board, row, column, color){
+    //exit condition
+    if(row < 3 || column > 3){
+        return;
+    } else {
+        //check if array contents up and to the right from (row, column) match the color of (row, column)
+        if(board[row - 1][column + 1] == color && board[row - 2][column + 2] == color &&  board[row - 3][column + 3] == color){
+            //color wins
+        } else {
+            return;
+        }
+    }
+}
+
+checkDownRightDiag(board, row, column, color){
+    //exit condition
+    if(row > 2 || column > 3){
+        return;
+    } else {
+        //check if array contents down and to the right from (row, column) match the color of (row, column)
+        if(board[row + 1][column + 1] == color && board[row + 2][column + 2] == color &&  board[row + 3][column + 3] == color){
+            //color wins
+        } else {
+            return;
+        }
+    }
+}
+
+checkDownLeftDiag(board, row, column, color){
+    //exit condition
+    if(row > 2 || column < 3){
+        return;
+    } else {
+        //check if array contents down and to the left from (row, column) match the color of (row, column)
+        if(board[row + 1][column - 1] == color && board[row + 2][column - 2] == color &&  board[row + 3][column - 3] == color){
+            //color wins
+        } else {
+            return;
+        }
+    }
+}
+
+checkUpLeftDiag(board, row, column, color){
+    //exit condition
+    if(row < 3 || column <3){
+        return;
+    } else {
+        if(board[row - 1][column - 1] == color && board[row - 2][column - 2] == color &&  board[row - 3][column - 3] == color){
+            //color wins
+        } else {
+            return;
+        }
+        //check if array contents up and to the left from (row, column) match the color of (row, column)
+    }
+}
