907e05e0ee726acdff6f8594c3d1fd7d4210b4a7
Peter Casey <casey.peter10@gmail.com>
2019-11-18 16:45:30 -0600

    modified:   connect_4.js 	modified:   gameStyle.css 	modified:   index.html 	deleted:    red_piece.png

--- connect_4.js
+++ connect_4.js
@@ -7,71 +7,76 @@ var gameBoard = [[0,0,0,0,0,0,0],
 
 var color = 1;
 var isGameOver = false;
+var AIPlayer = false;
 
 //bottom right corner of gameBoard accessed by [5][6]
 
 document.addEventListener("keydown", function(event){
     var key = event.key;
+
+    //Prevents dropping pieces if a player has won.
+    if(isGameOver == false){
+        
     
-    switch (key) {
-        case "1":
-            colTop = evalColumnHeight(gameBoard, 0);
-            dropPiece(0,colTop,color);
-            gameBoard[colTop][0] = color;
-            break;
-        case "2":
-            colTop = evalColumnHeight(gameBoard, 1);
-            dropPiece(1,colTop,color);
-            gameBoard[colTop][1] = color;
-            break;
-        case "3":
-            colTop = evalColumnHeight(gameBoard, 2);
-            dropPiece(2,colTop,color);
-            gameBoard[colTop][2] = color;
-            break;
-        case "4":
-            colTop = evalColumnHeight(gameBoard, 3);
-            dropPiece(3,colTop,color);
-            gameBoard[colTop][3] = color;
-            break;
-        case "5":
-            colTop = evalColumnHeight(gameBoard, 4);
-            dropPiece(4,colTop,color);
-            gameBoard[colTop][4] = color;
-            break;
-        case "6":
-            colTop = evalColumnHeight(gameBoard, 5);
-            dropPiece(5,colTop,color);
-            gameBoard[colTop][5] = color;
-            break;
-        case "7":
-            colTop = evalColumnHeight(gameBoard, 6);
-            dropPiece(6,colTop,color);
-            gameBoard[colTop][6] = color;
-            break;
+        switch (key) {
+            case "1":
+                colTop = evalColumnHeight(gameBoard, 0);
+                dropPiece(0,colTop,color);
+                gameBoard[colTop][0] = color;
+                break;
+            case "2":
+                colTop = evalColumnHeight(gameBoard, 1);
+                dropPiece(1,colTop,color);
+                gameBoard[colTop][1] = color;
+                break;
+            case "3":
+                colTop = evalColumnHeight(gameBoard, 2);
+                dropPiece(2,colTop,color);
+                gameBoard[colTop][2] = color;
+                break;
+            case "4":
+                colTop = evalColumnHeight(gameBoard, 3);
+                dropPiece(3,colTop,color);
+                gameBoard[colTop][3] = color;
+                break;
+            case "5":
+                colTop = evalColumnHeight(gameBoard, 4);
+                dropPiece(4,colTop,color);
+                gameBoard[colTop][4] = color;
+                break;
+            case "6":
+                colTop = evalColumnHeight(gameBoard, 5);
+                dropPiece(5,colTop,color);
+                gameBoard[colTop][5] = color;
+                break;
+            case "7":
+                colTop = evalColumnHeight(gameBoard, 6);
+                dropPiece(6,colTop,color);
+                gameBoard[colTop][6] = color;
+                break;
+            }
+
+        console.log("color is " + color);
+        consoleBoardPrint(gameBoard);
+        isGameOver = checkAllWins(gameBoard,color);
+        console.log("is game over " + isGameOver);
+            
+        if(isGameOver == true && color == 1){
+            console.log("red player has won.");
+            alert("Player 1 (red) wins");
         }
 
-    console.log("color is " + color);
-    consoleBoardPrint(gameBoard);
-    isGameOver = checkAllWins(gameBoard,color);
-    console.log("is game over " + isGameOver);
-        
-    if(isGameOver == true && color == 1){
-        console.log("red player has won.");
-        document.getElementById("currentPlayer").innerHTML = "Player 1 wins (Red)";
+        if(isGameOver == true && color == 2){
+            console.log("black player has won.");
+            alert("Player 2 (black) wins");
+        }
 
-    }
-
-    if(isGameOver == true && color == 2){
-        console.log("black player has won.");
-        document.getElementById("currentPlayer").innerHTML = "Player 2 wins (Black)";
-    }
-
-    //player switching 
-    if (color == 1){
-        color = 2;
-    } else {
-        color = 1;
+        //player switching 
+        if (color == 1){
+            color = 2;
+        } else {
+            color = 1;
+        }
     }
 }) //end of keydown event listener
 
@@ -136,37 +141,80 @@ involving up or left will be impossible, and will not be executed
 
 //all rows and columns are zero indexed
 
+function getColumnTops(gameBoardArray){
+    columnTopList = [0,0,0,0,0,0,0];
+    for(column = 0; column<7; column++){
+        columnTopList[column] = evalColumnHeight();
+    }
+    return columnTopList;
+}
+
+function checkBlockingAndWinningOpportunities(columnTopList, gameBoardArray, color){
+    for(column = 0; column<7; column++){
+        //getting winning opportunities
+        var winningBoardArray = gameBoardArray;
+
+        winningBoardArray[columnTopList[column]][column] = color;
+        var winningOpportunity = checkAllWins(winningBoardArray, color);
+        if (winningOpportunity == true){
+            //if placing a piece in the "scanned" column, and a win would result, the column will be returned.
+            return column;
+        }
+
+        if(winningOpportunity == False){
+            if(color == 1){
+                color = 2;
+            } else {
+                color = 1;
+            }
+
+            var blockingBoardArray = gameBoardArray;
+            var blockingOpportunity = checkAllWins(blockingBoardArray);
+            
+            //if placing a piece in the "scanned" column, and a win would be blocked, the column will be returned.
+            if (blockingOpportunity == true){
+                return column;
+            } else {
+                //if no blocking or winning opportunities are available, select
+                return Math.floor((Math.random() * 7) + 1);
+            }
+        }
+    }
+}
+
 function checkAllWins(board, color){
     var playerWon = false;
     console.log("in checkAllWins");
     
     for(row = 0; row<6; row++){
         for(column = 0; column<7; column++){
-            console.log("checking cell in row s" + row + " column " + column);
-            playerWon = checkUpWin(board, row, column, color);
-            if(playerWon == true){ return playerWon;}
-
-            playerWon = checkDownWin(board, row, column, color);
-            if(playerWon == true){ return playerWon;}
             
-            playerWon = checkLeftWin(board, row, column, color);
-            if(playerWon == true){ return playerWon;}
+            if(board[row][column] != 0){
+                console.log("checking cell in row s" + row + " column " + column);
+                playerWon = checkUpWin(board, row, column, color);
+                if(playerWon == true){ return playerWon;}
 
-            playerWon = checkRightWin(board, row, column, color);
-            if(playerWon == true){ return playerWon;}
+                playerWon = checkDownWin(board, row, column, color);
+                if(playerWon == true){ return playerWon;}
+                
+                playerWon = checkLeftWin(board, row, column, color);
+                if(playerWon == true){ return playerWon;}
 
-            playerWon = checkUpRightDiagWin(board, row, column, color);
-            if(playerWon == true){ return playerWon;}
+                playerWon = checkRightWin(board, row, column, color);
+                if(playerWon == true){ return playerWon;}
 
-            playerWon = checkDownRightDiagWin(board, row, column, color);
-            if(playerWon == true){ return playerWon;}
+                playerWon = checkUpRightDiagWin(board, row, column, color);
+                if(playerWon == true){ return playerWon;}
 
-            playerWon = checkDownLeftDiagWin(board, row, column, color);
-            if(playerWon == true){ return playerWon;}
+                playerWon = checkDownRightDiagWin(board, row, column, color);
+                if(playerWon == true){ return playerWon;}
 
-            playerWon = checkUpLeftDiagWin(board, row, column, color);
-            if(playerWon == true){ return playerWon;}
-            
+                playerWon = checkDownLeftDiagWin(board, row, column, color);
+                if(playerWon == true){ return playerWon;}
+
+                playerWon = checkUpLeftDiagWin(board, row, column, color);
+                if(playerWon == true){ return playerWon;}
+            }
         }
     }
     return false;  
